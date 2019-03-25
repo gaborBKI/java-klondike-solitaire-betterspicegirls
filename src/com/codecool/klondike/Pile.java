e07a24316fadc20a21c1b0212b0a1e6e9d510b77
gaborBKI <gabor.bki@gmail.com>
2019-03-25 15:48:23 +0100

    Pile TODOs done.

--- src/com/codecool/klondike/Pile.java
+++ src/com/codecool/klondike/Pile.java
@@ -40,8 +40,7 @@ public class Pile extends Pane {
     }
 
     public int numOfCards() {
-        //TODO
-        return 1;
+        return cards.size();
     }
 
     public boolean isEmpty() {
@@ -49,7 +48,7 @@ public class Pile extends Pane {
     }
 
     public void clear() {
-        //TODO
+        cards.clear();
     }
 
     public void addCard(Card card) {
