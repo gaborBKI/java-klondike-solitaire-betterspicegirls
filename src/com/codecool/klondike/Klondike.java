9d254142d51b6687d3b36eb05c42aa1d02512c58
Geiszlinger Gergely <ggergely@gmail.com>
2019-03-27 13:37:20 +0100

    Restart option added!

--- src/com/codecool/klondike/Klondike.java
+++ src/com/codecool/klondike/Klondike.java
@@ -2,8 +2,13 @@ package com.codecool.klondike;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
+import javafx.event.EventHandler;
 
 public class Klondike extends Application {
 
@@ -17,12 +22,27 @@ public class Klondike extends Application {
     @Override
     public void start(Stage primaryStage) {
         Card.loadCardImages();
+        Button restart = new Button("Restart");
+        restart.setLayoutX(600);
+        restart.setLayoutY(650);
+        restart.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent mouseEvent){
+                if(mouseEvent.getButton().equals(MouseButton.PRIMARY)){
+                    start(primaryStage);
+                    System.out.println("Yaay first step taken");
+                }
+            }
+            });
         Game game = new Game();
+        game.getChildren().add(restart);
         game.setTableBackground(new Image("/table/green.png"));
+
 
         primaryStage.setTitle("Klondike Solitaire");
         primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
         primaryStage.show();
+
     }
 
 }
